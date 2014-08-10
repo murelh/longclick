4db08676886a5b0dc9553ee4d1f79df657b45a9e
murelh <murelh@live.fr>
2014-08-11 00:15:37 +0200

    Create longclick.js

---
+++ longclick.js
@@ -0,0 +1,33 @@
+/*
+	fonction longclick créee et partagée sous licence libre par
+	Ntyandi Murelh
+	http://murelh.info
+	Développeur web
+*/
+
+jQuery.fn.extend({
+    longclick: function (fn, duration, call)
+	{
+		var duration = ($.isNumeric(duration) && !(duration < 0))? duration: 1000;
+		var callbacks = $.Callbacks();
+		if($.isFunction(call))
+		{
+			callbacks.add(call);
+		}
+		else if(call != undefined)
+		{
+			console.warn('Les istructions de retour de la fonction $.longclick(fn, duration, callback) ne pourront pas être exécutées car elles ne font pas partie d\'une fonction. Ci-dessous, l\'objet concerné:');
+			console.log(this);
+		}
+		
+        $(this).on('mousedown',function(e) // au clique sur l'élément
+		{
+			setTimeout(function()
+			{
+				fn();
+				callbacks.fire(call);
+			},
+			duration);
+		});
+	}
+});
